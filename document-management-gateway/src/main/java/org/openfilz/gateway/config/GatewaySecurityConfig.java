7107ee8bfab95d7ab501113102f7fc627fa3978d
Yann <yanndemel78@gmail.com>
2025-07-09 15:00:54 +0200

    tests config

--- document-management-gateway/src/main/java/org/openfilz/gateway/config/GatewaySecurityConfig.java
+++ document-management-gateway/src/main/java/org/openfilz/gateway/config/GatewaySecurityConfig.java
@@ -1,14 +1,12 @@
 // com/example/gateway/config/GatewaySecurityConfig.java
 package org.openfilz.gateway.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 
@@ -20,8 +18,12 @@ public class GatewaySecurityConfig {
 
     // Define public paths that don't require authentication at the gateway level
     private static final String[] PUBLIC_PATHS = {
-        "/actuator/**", // Allow actuator endpoints
-        // Add other public paths if needed, e.g., specific API docs for the gateway itself
+        "/actuator/**", // Allow actuator endpoint
+            "*/v3/api-docs/**",
+            "*/swagger-ui/**",
+            "*/swagger-ui.html",
+            "*/webjars/swagger-ui/**"
+            // Add other public paths if needed, e.g., specific API docs for the gateway itself
         // Note: The downstream service (DMS API) will still enforce its own security for its Swagger UI.
     };
 
@@ -29,6 +31,7 @@ public class GatewaySecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
+            .cors(ServerHttpSecurity.CorsSpec::disable)
             .csrf(ServerHttpSecurity.CsrfSpec::disable) // Standard for stateless APIs
             .authorizeExchange(exchange -> exchange
                 .pathMatchers(PUBLIC_PATHS).permitAll() // Public paths
