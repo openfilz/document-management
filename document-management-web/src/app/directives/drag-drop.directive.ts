b026fdd21228e554baa061f3924f50678d91cadc
Yann <yanndemel78@gmail.com>
2025-08-06 11:37:16 +0200

    Improve UI : overlay flash issue solved

--- document-management-web/src/app/directives/drag-drop.directive.ts
+++ document-management-web/src/app/directives/drag-drop.directive.ts
@@ -9,20 +9,34 @@ export class DragDropDirective {
   @Output() fileOverChange = new EventEmitter<boolean>();
   @HostBinding('class.file-over') fileOver: boolean = false;
 
+  private counter = 0;
+
+  @HostListener('dragenter', ['$event'])
+  onDragEnter(evt: DragEvent) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    this.counter++;
+    if (!this.fileOver) {
+      this.fileOver = true;
+      this.fileOverChange.emit(true);
+    }
+  }
+
   @HostListener('dragover', ['$event'])
   onDragOver(evt: DragEvent) {
     evt.preventDefault();
     evt.stopPropagation();
-    this.fileOver = true;
-    this.fileOverChange.emit(true);
   }
 
   @HostListener('dragleave', ['$event'])
   onDragLeave(evt: DragEvent) {
     evt.preventDefault();
     evt.stopPropagation();
-    this.fileOver = false;
-    this.fileOverChange.emit(false);
+    this.counter--;
+    if (this.counter === 0) {
+      this.fileOver = false;
+      this.fileOverChange.emit(false);
+    }
   }
 
   @HostListener('drop', ['$event'])
@@ -31,6 +45,7 @@ export class DragDropDirective {
     evt.stopPropagation();
     this.fileOver = false;
     this.fileOverChange.emit(false);
+    this.counter = 0;
     if (evt.dataTransfer?.files) {
       this.filesDropped.emit(evt.dataTransfer.files);
     }
